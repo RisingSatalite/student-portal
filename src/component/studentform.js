6d61b9f18fb71db1c564d126e830c96c5a87e441
Saihaj Mann <saihajmann07@gmail.com>
2023-10-27 21:37:37 -0600

    Update studentform.js

--- src/component/studentform.js
+++ src/component/studentform.js
@@ -1,1 +1,27 @@
 //create form to add student here
+import React from 'react'
+import { useEffec, useState } from 'react'
+
+const NewStudentForm = () => {
+    const [registrationForm, setRegistrationForm] = React.useState([])
+
+    const AddStudent = (firstName, lastName, birthday, grade) =>{
+        fetch('http://localhost:5000/students', {
+            method: 'POST',
+            header: {'content-type': 'application/json'},
+            body: JSON.stringify({firstName, lastName, birthday, grade})
+        })
+    }
+
+    return (
+        <form>
+            <input className='border p-2' type='text' name='firstName' placeholder="Enter student's first name here"></input>
+            <input className='border p-2' type='text' name='lastName' placeholder="Enter student's last name here"></input>
+            <input className='border p-2' type='date' name='birthday' placeholder="Enter student's birthday here"></input>
+            <input className='border p-2' type='number' name='grade' placeholder="Enter student's grade here"></input>
+            <button className='bg-blue-500 text-white py-1 px-2 rounded' type='submit'>Submit data</button>
+        </form>
+    )
+
+}
+export default NewStudentForm
