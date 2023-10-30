f5649937f19fd7f0d3cf500f73e72da4c09a943e
Saihaj Mann <saihajmann07@gmail.com>
2023-10-29 20:35:18 -0600

    more changes

--- src/component/studentdisplay.js
+++ src/component/studentdisplay.js
@@ -1,38 +1,32 @@
-//display students here, also call
-'use client'
+import React, { useEffect, useState } from 'react';
 
-import React from 'react'
-import { useEffect, useState } from 'react'
-
-//get
-
-function Studentdisplay() {
-  const [studentData, setStudentData] = useState([])
+function StudentDisplay() {
+  const [studentData, setStudentData] = useState([]);
 
   useEffect(() => {
-    fetch("https://localhost:5000/student")
-    .then(response =>response.json)
-    .then(data => setStudentData(data))
+    fetch('http://localhost:5000/student')
+      .then((response) => response.json())
+      .then((data) => setStudentData(data))
+      .catch((error) => console.error('Error fetching data:', error));
   }, []);
 
-    return (
-      <div className="">
-        All students here
-        <ul className='mt-4'>
-          {studentData.map(post => {
-            <li key={post.id} className='border p-2 flex justify-between items-center'>
-              <div className='flex items-center'>
-                <span>{post.firstName}</span>
-                <span>{post.lastName}</span>
-                <span>{post.birthday}</span>
-                <span>{post.grade}</span>
+  return (
+    <div className="">
+      <p>All students here</p>
+      <ul className='mt-4'>
+        {studentData.map((post) => (
+          <li key={post.id} className='border p-2 flex justify-between items-center'>
+            <div className='flex items-center'>
+              <span>{post.firstName}</span>
+              <span>{post.lastName}</span>
+              <span>{post.birthday}</span>
+              <span>{post.grade}</span>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 
-              </div>
-            </li>
-          })}
-        </ul>
-      </div>
-    )
-  }
-
-export default Studentdisplay
+export default StudentDisplay;
