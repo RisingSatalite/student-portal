f9d7619319ff7ace6f0e72c8b9738901eceae654
Saihaj Mann <saihajmann07@gmail.com>
2023-11-06 08:48:27 -0700

    changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,12 @@
-import TabManager from "@/component/tabmanager"
-import Footer from "@/component/footer"
+import TabManager from "@/components/tabmanager"
+import Footer from "@/components/footer"
 
 import 'tailwindcss/tailwind.css';
 
 export default function Home() {
   return (
     <main className="">
-      <div className="text-yellow-300">tests</div>
+      
       <TabManager></TabManager>
       <br/>
       <Footer></Footer>
