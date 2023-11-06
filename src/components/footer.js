d4b4bf05c8284d1daa035bd4373947603545f277
Saihaj Mann <saihajmann07@gmail.com>
2023-11-06 08:58:45 -0700

    some tailwindcss changes

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@
 const Footer = () => {
     //THis is the footer add more infromation about the school here
     return <footer class="bg-gray-400" className="text-black bg-gray-400">
-        <div className="bg-blue-400">school info, legal ,etc</div>
+        <div className="">School info, legal ,etc</div>
         <div>Thank you using "insert school name here" school system</div>
     </footer>
 
