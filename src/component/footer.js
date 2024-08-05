89182f0d25523096c8c3bfc1a9b8406c666832fe
Saihaj Mann <saihajmann07@gmail.com>
2024-08-05 17:59:48 -0600

    fix quotes

--- src/component/footer.js
+++ src/component/footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     //THis is the footer add more infromation about the school here
     return <footer class="bg-gray-400" className="text-black bg-gray-400">
         <div className="bg-blue-400">school info, legal ,etc</div>
-        <div>Thank you using "insert school name here" school system</div>
+        <div>Thank you using &quot;insert school name here&quot; school system</div>
     </footer>
 
 }
