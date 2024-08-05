8378e496eaf5791606a0243603e0934277fc6973
Saihaj Mann <saihajmann07@gmail.com>
2024-08-05 17:56:57 -0600

    add id and fix error?

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,9 +2,9 @@
 
 const Footer = () => {
     //THis is the footer add more infromation about the school here
-    return <footer className="text-black">
-        <div className="">School info, legal ,etc</div>
-        <div>Thank you using "insert school name here" school system</div>
+    return <footer className='text-black'>
+        <div>School info, legal ,etc</div>
+        <div>Thank you using &quot;insert school name here&quot; school system</div>
     </footer>
 
 }
